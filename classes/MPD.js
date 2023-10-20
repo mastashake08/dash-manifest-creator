59b846df196c937551f70e134a11d29a8a62cdc0
Hacker God <jyrone.parker@gmail.com>
2023-10-20 00:10:44 -0400

    move to generator functions 2.0.0-alpha

--- classes/MPD.js
+++ classes/MPD.js
@@ -28,50 +28,74 @@ class MPD {
     this.mpd = doc.createElement('MPD')
   }
 
-  createMpd (videoData = this.video, mediaUrl = '', startNumber = 1) {
-    const date = new Date(Date.now()).toISOString()
-    this.mpd.setAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance')
-    this.mpd.setAttribute('xmlns', 'urn:mpeg:dash:schema:mpd:2011')
-    this.mpd.setAttribute('xsi:schemaLocation', 'urn:mpeg:dash:schema:mpd:2011 DASH-MPD.xsd')
-    this.mpd.setAttribute('type', 'dynamic')
-    this.mpd.setAttribute('profiles', 'urn:mpeg:dash:profile:isoff-live:2011')
-    this.mpd.setAttribute('minimumUpdatePeriod', 'PT60S')
-    this.mpd.setAttribute('minBufferTime', 'PT12S')
-    this.mpd.setAttribute('availabilityStartTime', date)
-    const period = this.createPeriod(this.mpd)
-    const as = this.createAdaptationSet(period)
-    //const cc = this.createContentComponent(as)
-    const st = this.createSegmentTemplate(as, "1000", "2000", startNumber, videoData, mediaUrl)
-    return this.doc
+  *createMpd (videoData = this.video, mediaUrl = '', startNumber = 1) {
+    while(true) {
+      const date = new Date(Date.now()).toISOString()
+      this.mpd.setAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance')
+      this.mpd.setAttribute('xmlns', 'urn:mpeg:dash:schema:mpd:2011')
+      this.mpd.setAttribute('xsi:schemaLocation', 'urn:mpeg:dash:schema:mpd:2011 DASH-MPD.xsd')
+      this.mpd.setAttribute('type', 'dynamic')
+      this.mpd.setAttribute('profiles', 'urn:mpeg:dash:profile:isoff-live:2011')
+      this.mpd.setAttribute('minimumUpdatePeriod', 'PT60S')
+      this.mpd.setAttribute('minBufferTime', 'PT12S')
+      this.mpd.setAttribute('availabilityStartTime', date)
+      const period = this.createPeriod(this.mpd)
+      const as = this.createAdaptationSet(period)
+      const cc = this.createContentComponent(as)
+      const st = this.createSegmentTemplate(as, "1000", "2000", startNumber, videoData, mediaUrl)
+      console.log(period.next().value)
+      console.log(as.next().value)
+      console.log(cc.next().value)
+      console.log(st.next().value)
+      yield this.doc
+    }
   }
 
-  createPeriod (el) {
+  /**
+   * 
+   * @param {*} el - element that will be appended to
+   * @param {*} timeOffset - time offset for start time
+   */
+  *createPeriod (el, timeOffset) {
+    let id = 1;
+    let time = 0;
     const period = this.doc.createElement('Period')
-    period.setAttribute('start', 'PT0S')
-    period.setAttribute('id', "1")
-    el.appendChild(period)
-    return period
+    while (true) {
+      if (time == 0) {
+        period.setAttribute('start', `PT${time}S`)
+      } else {
+        period.setAttribute('start', `PT${time + timeOffset}S`)
+      }
+      period.setAttribute('id', id)
+      yield el.appendChild(period)
+      id++;
+    }
   }
 
-  createAdaptationSet (el, mimetype = 'video/webm') {
+  *createAdaptationSet (el, mimetype = 'video/webm') {
+    while(true) {
     const as = this.doc.createElement('AdaptationSet')
     as.setAttribute('mimeType', mimetype)
-    el.appendChild(as)
-    return as
+    yield el.appendChild(as)
+    }
+   
   }
 
-  createContentComponent (el, contentType = 'video', id = "1") {
-    const cc = this.doc.createElement('ContentComponent')
-    cc.setAttribute('contentType', contentType)
-    cc.setAttribute('id', id)
-    el.appendChild(cc)
-    return cc
+  *createContentComponent (el, contentType = 'video') {
+    let id = 1;
+    while(true) {
+      const cc = this.doc.createElement('ContentComponent');
+      cc.setAttribute('contentType', contentType);
+      cc.setAttribute('id', id);
+      yield el.appendChild(cc);
+      id++;
+    }
   }
 
-  createSegmentTemplate (el, timescale = "1000", duration = "2000", startNumber = "1", videoData = this.video, media = '') {
+  *createSegmentTemplate (el, timescale = "1000", duration = "2000", startNumber = "1", videoData = this.video, media = '') {
     const st = this.doc.createElement('SegmentTemplate')
     st.setAttribute('startNumber', startNumber)
-    st.setAttribute('initialization', "data:video/webm;base64,"+btoa(videoData))
+    st.setAttribute('initialization', "data:video/webm;base64,"+btoa(yield videoData))
     st.setAttribute('media', media)
     el.appendChild(st)
     return st
@@ -88,6 +112,7 @@ class MPD {
     const file = new Blob(['<?xml version="1.0" encoding="UTF-8"?>'+str], {
       type: "application/xml",
     })
+    console.log(file)
     return file
   }
 }
